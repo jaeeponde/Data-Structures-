f205496b792ba3cffdd43d7faa5c3fca3379a1cc
jaeeponde <jaeeponde@gmail.com>
2023-09-24 16:09:19 +0530

    deleted

--- Extra Credit/arraypermutations.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-/*Logic of the algorithm :
-Basically, you fix an element and then constantly decrease the array size 
-as you permutate the remaining elements.
-It uses a while loop to keep track of the count of swaps 
-When the number of swaps =  n, a new permutation has been generated*/
-
