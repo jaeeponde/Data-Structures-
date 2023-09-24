e793b47855c9bd89d8536e4a42c246054ed498c6
jaeeponde <jaeeponde@gmail.com>
2023-09-24 13:23:11 +0530

    changes

---
+++ Structures.c/linkedstack.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define BADVAL = -999
+
+struct node{
+int info;
+struct node *next;
+};
+typedef struct node * NODEPTR;
+NODEPTR createNode(int val){
+NODEPTR p;
+p = (NODEPTR) malloc(sizeof(struct node));
+p->info = val;
+p->next = NULL;
+return p;
+}
+int main(){
+NODEPTR p, q, temp;
+p = createNode(5);
+q = createNode(6);
+p->next = q;
+q = NULL;
+q = createNode(7);
+(p->next)->next = q;
+for(temp = p; temp!= NULL; temp = temp->next)
+printf("%d %x %x\n", temp->info, (unsigned int) temp, (unsigned int) temp->next);
+
+}
+
+
+int isEmpty(NODEPTR p){
+if (p == NULL) return 1;
+else return 0;
+}
+int pop(NODEPTR *ap){
+NODEPTR temp;
+int val;
+if (isEmpty(*ap)) return BADVAL;
+else{
+temp = *ap;
+(*ap) = (*ap)->next;
+val = temp->info;
+free(temp);
+return val;
+}
+}
+
+void push(NODEPTR *ap, int elm){
+NODEPTR temp;
+temp = createNode(elm);
+if (*ap == NULL)
+*ap = temp;
+else{
+temp->next = *ap;
+*ap = temp;
+}
+}
+void printStack(NODEPTR p){
+NODEPTR temp;
+if (isEmpty(p)){
+printf("Stack empty\n");
+return;
+}
+for (temp = p; temp!= NULL; temp = temp->next)
+printf("%d\n", temp->info);
+
+}
+
+int main(){
+NODEPTR st;
+int elm, menu;
+while(1){
+printf("0. exit, 1. initialize, 2. pop, 3. push, 4. print. Input Choice: ");
+scanf("%d", &menu);
+switch(menu){
+case 0: exit(0);
+case 1: st = NULL;
+break;
+case 2: printf("Popped Value %d\n", pop(&st));
+break;
+case 3: printf("Give element: ");
+scanf("%d", &elm);
+push(&st, elm);
+break;
+case 4: printStack(st);
+break;
+default: printf("Wrong choice\n");
+}
+}
+}
+
+
